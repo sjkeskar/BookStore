f1e0dd0e99002bc5bdf885eff447f48ff28482d2
sjkeskar <sjkeskar@gmail.com>
2020-12-18 21:28:34 +0530

    routes for address operations

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { logInUser, registerUser } from "../controller/usercontroller.js";
+import {
+	getUserInfo,
+	logInUser,
+	registerUser,
+} from "../controller/usercontroller.js";
+import { getAllAdd, newAdd } from "../controller/addresscontroller.js";
+import { protect } from "../middleware/authMiddleware.js";
 
+router.route("/").get(protect, getUserInfo);
 router.route("/register").post(registerUser);
 router.route("/login").post(logInUser);
+router.route("/add").get(protect, getAllAdd).post(protect, newAdd);
 export default router;
