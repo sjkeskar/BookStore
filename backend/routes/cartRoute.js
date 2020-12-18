3334b8966527a43ddbc5e4afd3963c7734b0e258
sjkeskar <sjkeskar@gmail.com>
2020-12-18 19:10:25 +0530

    routes for updating and deleting

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { addBooktoCart } from "../controller/cartcontroller.js";
+import {
+	addBooktoCart,
+	delBooks,
+	getBooks,
+	updateBooks,
+} from "../controller/cartcontroller.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.route("/").post(protect, addBooktoCart)
+router
+	.route("/")
+	.post(protect, addBooktoCart)
+	.get(protect, getBooks)
+	.put(protect, updateBooks);
 
+router.route("/del").post(protect, delBooks);
 export default router;
