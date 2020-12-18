2e42e4cf97b077c0b68e87e0939eec773ca8c4eb
sjkeskar <sjkeskar@gmail.com>
2020-12-18 19:13:42 +0530

    completed all cart reducers

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,14 +1,66 @@
-import { ADD_BOOK_FAIL, ADD_BOOK_SUCCESS } from "../constants/cartConstants";
+import {
+	CART_ADD_BOOK_FAIL,
+	CART_ADD_BOOK_REQUEST,
+	CART_ADD_BOOK_SUCCESS,
+	CART_GET_BOOK_FAIL,
+	CART_GET_BOOK_REQUEST,
+	CART_GET_BOOK_SUCCESS,
+	CART_REMOVE_BOOK_FAIL,
+	CART_REMOVE_BOOK_REQUEST,
+	CART_REMOVE_BOOK_SUCCESS,
+	CART_UPDATE_BOOK_FAIL,
+	CART_UPDATE_BOOK_REQUEST,
+	CART_UPDATE_BOOK_SUCCESS,
+} from "../constants/cartConstants";
 
 export const addBookCartReducer = (state = {}, action) => {
 	switch (action.type) {
-		case ADD_BOOK_REQUEST:
+		case CART_ADD_BOOK_REQUEST:
 			return { loading: true };
-		case ADD_BOOK_SUCCESS:
+		case CART_ADD_BOOK_SUCCESS:
 			return { loading: false, success: true, cartItems: action.payload };
-		case ADD_BOOK_FAIL:
+		case CART_ADD_BOOK_FAIL:
 			return { loading: false, success: false, error: action.payload };
 		default:
 			return state;
 	}
 };
+
+export const getCartBooksReducer = (state = { cartItems: [] }, action) => {
+	switch (action.type) {
+		case CART_GET_BOOK_REQUEST:
+			return { loading: true };
+		case CART_GET_BOOK_SUCCESS:
+			return { loading: false, cartItems: action.payload };
+		case CART_GET_BOOK_FAIL:
+			return { loading: false, error: action.payload };
+		default:
+			return state;
+	}
+};
+
+export const updateQuantityReducer = (state={}, action) =>{
+	switch(action.type){
+		case CART_UPDATE_BOOK_REQUEST:
+			return {loading: true}
+		case CART_UPDATE_BOOK_SUCCESS:
+			return {loading: true, success: true}
+		case CART_UPDATE_BOOK_FAIL:
+			return {loading: true, success: false}
+		default:
+			return {}
+	}
+}
+
+export const removeCartBookReducer = (state = {}, action)=>{
+	switch(action.type){
+		case CART_REMOVE_BOOK_REQUEST:
+			return {loading: true}
+		case CART_REMOVE_BOOK_SUCCESS:
+			return {loading: false, success: true}
+		case CART_REMOVE_BOOK_FAIL:
+			return {loading: false, success: false}
+		default:
+			return {}
+	}
+}
