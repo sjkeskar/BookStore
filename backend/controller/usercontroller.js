510c836dc8b3d562631a1dae61db3441bdcd223c
sjkeskar <sjkeskar@gmail.com>
2020-12-18 21:28:04 +0530

    added function for getting userInfo

--- backend/controller/usercontroller.js
+++ backend/controller/usercontroller.js
@@ -40,7 +40,7 @@ export const registerUser = asyncHandler(async (req, res) => {
 //@access   Public
 export const logInUser = asyncHandler(async (req, res) => {
 	const { EmailID, Password } = req.body;
-	const sql = `SELECT * FROM user WHERE EmailID='${EmailID}'`;
+	const sql = `SELECT * FROM user WHERE EmailID='${EmailID}';`;
 	db.query( sql, async (error, result) => {
 		if (error) {
 			res.status(500);
@@ -62,16 +62,22 @@ export const logInUser = asyncHandler(async (req, res) => {
 			}
 		}
 	});
-	// if (user && (await user.matchPassword(password))) {
-	// 	res.json({
-	// 		_id: user._id,
-	// 		name: user.name,
-	// 		email: user.email,
-	// 		isAdmin: user.isAdmin,
-	// 		token: generateToken(user._id),
-	// 	});
-	// } else {
-	// 	res.status(401);
-	// 	throw new Error("Invalid Email or Password");
-	// }
 });
+
+//@desc 	Get User Info
+//@route	GET /api/users
+//@access	private
+export const getUserInfo = asyncHandler(async(req,res)=>{
+	const {UserID} = req.user;
+	const sql = `SELECT UserID,FirstName,LastName,EmailID,PhoneNo FROM user WHERE UserID=${UserID};`
+	db.query(sql,(error, result)=>{
+		if(error){
+			res.status(501)
+			throw new Error(error)
+		}else{
+			result = JSON.parse(JSON.stringify(result))
+			res.status(200)
+			res.json(result)
+		}
+	})
+})
