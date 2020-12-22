784ba7e3bded3207e3a712942a998b4050e344af
sjkeskar <sjkeskar@gmail.com>
2020-12-22 22:49:45 +0530

    added controller for getting single address

--- backend/controller/addresscontroller.js
+++ backend/controller/addresscontroller.js
@@ -19,14 +19,43 @@ export const getAllAdd = asyncHandler(async (req, res) => {
 	});
 });
 
+//@desc     Get one address corresponding to an user
+//@route    GET /api/users/add/:id
+//@access   Private
+export const getOneAdd = asyncHandler(async (req, res) => {
+	const { UserID } = req.user;
+	const sql = `SELECT * FROM address WHERE UserID=${UserID} AND AddID=${req.params.id};`;
+	db.query(sql, (error, result) => {
+		if (error) {
+			res.status(501);
+			throw new Error(error);
+		} else {
+			result = JSON.parse(JSON.stringify(result));
+			res.status(200);
+			res.json(result);
+		}
+	});
+});
+
 //@desc     Add an address to the database
 //@route    POST /api/users/add
 //@access   Private
 export const newAdd = asyncHandler(async (req, res) => {
-    const AddID = Math.floor(Math.random() * 1000);
-    const { UserID } = req.user;
-    const {Flatno, Building, Street, Landmark, Town, District, State, Country, PostalCode, Default} = req.body;
-	const sql = `INSERT INTO address VALUES(${AddID}, ${Flatno}, '${Building}','${Street}','${Landmark}','${Town}','${District}','${State}','${Country}',${PostalCode},${Default},${UserID});`;
+	const AddID = Math.floor(Math.random() * 1000);
+	const { UserID } = req.user;
+	const {
+		Flatno,
+		Building,
+		Street,
+		Landmark,
+		Town,
+		District,
+		State,
+		Country,
+		PostalCode,
+		Default,
+	} = req.body;
+	const sql = `INSERT INTO address VALUES(${AddID}, ${Flatno}, '${Building}','${Street}','${Landmark}','${Town}','${District}','${State}','${Country}',${PostalCode},'${Default}',${UserID});`;
 	db.query(sql, (error, result) => {
 		if (error) {
 			res.status(501);
