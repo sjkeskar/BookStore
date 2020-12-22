c17aa91ff66afea818fb31b235b84b83f9b9c7f7
sjkeskar <sjkeskar@gmail.com>
2020-12-22 22:51:19 +0530

    routes for new user and address controllers

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,12 +4,18 @@ import {
 	getUserInfo,
 	logInUser,
 	registerUser,
+	updateUserInfo,
 } from "../controller/usercontroller.js";
-import { getAllAdd, newAdd } from "../controller/addresscontroller.js";
+import {
+	getAllAdd,
+	getOneAdd,
+	newAdd,
+} from "../controller/addresscontroller.js";
 import { protect } from "../middleware/authMiddleware.js";
 
-router.route("/").get(protect, getUserInfo);
+router.route("/").get(protect, getUserInfo).post(protect, updateUserInfo);
 router.route("/register").post(registerUser);
 router.route("/login").post(logInUser);
 router.route("/add").get(protect, getAllAdd).post(protect, newAdd);
+router.route("/add/:id").get(protect, getOneAdd);
 export default router;
