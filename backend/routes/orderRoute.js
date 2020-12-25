b128981880a353634a6e3da8edd4656596b9cc7c
sjkeskar <sjkeskar@gmail.com>
2020-12-25 09:39:31 +0530

    order routes

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+const router = express.Router();
+import { protect } from "../middleware/authMiddleware.js";
+import {
+	addOrder,
+	getOrders,
+	infoexch,
+} from "../controller/ordercontroller.js";
+
+router.route("/").post(protect, addOrder).get(protect, getOrders).put(infoexch);
+
+export default router;
