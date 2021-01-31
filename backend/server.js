4d106d324b642a578dd598b76fffc9f31f8ede1d
sjkeskar <sjkeskar@gmail.com>
2021-01-31 18:27:28 +0530

    static folder for uploads added

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import db from "./config/db.js";
 import bookRoute from "./routes/bookRoute.js";
 import userRoute from "./routes/userRoute.js";
 import cartRoute from "./routes/cartRoute.js";
 import orderRoute from "./routes/orderRoute.js";
+import uploadRoute from "./routes/uploadRoute.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+
 db.connect(() => console.log(`connected to DB`));
 
 app.use("/api/books", bookRoute);
 app.use("/api/users", userRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/order", orderRoute);
+app.use("/api/upload", uploadRoute);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
 app.use(notFound);
 app.use(errorHandler);
 
