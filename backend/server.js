8093007a3fda020577f69eb0788d9056713ae349
sjkeskar <sjkeskar@gmail.com>
2021-02-17 15:12:23 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -26,11 +26,26 @@ app.use("/api/upload", uploadRoute);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+	});
+} else {
+	app.get("/", (req, res) => {
+		res.send(`api is running....`);
+	});
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || localhost;
 app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} on Port ${PORT}`)
+	HOST,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} on Port ${PORT} on HOST ${HOST}`
+	)
 );
